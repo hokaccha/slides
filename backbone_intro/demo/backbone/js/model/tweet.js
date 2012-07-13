c4e48c9eb7bebd6f3f3c95ad7dd8598851200037
hokaccha <k.hokamura@gmail.com>
2012-07-13 11:20:03 +0900

    Add backbone slide

--- backbone_intro/demo/backbone/js/model/tweet.js
+++ backbone_intro/demo/backbone/js/model/tweet.js
@@ -3,6 +3,13 @@
 var Model = TwitterSearch.Model;
 
 Model.Tweet = Model.Base.extend({
+  set: function(attrs, opts) {
+    if (attrs.text) {
+      attrs.text_linked = twttr.txt.autoLink(attrs.text);
+    }
+
+    return Model.Base.prototype.set.call(this, attrs, opts);
+  }
 });
 
 })();
